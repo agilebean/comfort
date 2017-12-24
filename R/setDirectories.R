fac73306a1813fa07b2d53aea5a7029f1ffd074a
Agile Bean <chaehan.so@gmail.com>
2017-12-25 02:32:24 +0900

    replace prefix with function call get_prefix()

--- R/setDirectories.R
+++ R/setDirectories.R
@@ -1,21 +1,19 @@
 
-dir_prefix <- function()
+# get path directory get_prefix(), e.g. /Users/Chaehan or /home/chaehan
+get_prefix <- function()
 {
   prefix <- gsub("^(/.*/.*?)/.*", "\\1", getwd())
   return(prefix)
 }
 
-# get path directory prefix, e.g. /Users/Chaehan or /home/chaehan
-prefix <- dir_prefix()
-
 # set directories
-credentialsDir <- paste0(prefix, "/Dropbox/06 Machine Learning/06 ML Scripts/_credentials")
-inputML <- paste0(prefix, "/Dropbox/06 Machine Learning/06 ML Scripts/_data/")
-outputML <- paste0(prefix, "/Dropbox/06 Machine Learning/06 ML Scripts/_output/")
-inputDV <- paste0(prefix, "/Google Drive/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_input")
-outputDV <- paste0(prefix, "/Google Drive/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_output")
-inputTP <- paste0(prefix, "/Google Drive/03 Publishing/21 Time Pressure/_input")
-outputTP <- paste0(prefix, "/Google Drive/03 Publishing/21 Time Pressure/_output")
+credentialsDir <- paste0(get_prefix(), "/Dropbox/06 Machine Learning/06 ML Scripts/_credentials")
+inputML <- paste0(get_prefix(), "/Dropbox/06 Machine Learning/06 ML Scripts/_data/")
+outputML <- paste0(get_prefix(), "/Dropbox/06 Machine Learning/06 ML Scripts/_output/")
+inputDV <- paste0(get_prefix(), "/Google Drive/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_input")
+outputDV <- paste0(get_prefix(), "/Google Drive/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_output")
+inputTP <- paste0(get_prefix(), "/Google Drive/03 Publishing/21 Time Pressure/_input")
+outputTP <- paste0(get_prefix(), "/Google Drive/03 Publishing/21 Time Pressure/_output")
 
 
 dirs <- list(credentialsDir,
