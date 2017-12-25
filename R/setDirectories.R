5b25758504b77b7a3d98a2abc75863ffa7d44b94
Agile Bean <chaehan.so@gmail.com>
2017-12-25 19:03:41 +0900

    set local path base for /private/var

--- R/setDirectories.R
+++ R/setDirectories.R
@@ -1,27 +1,27 @@
 
-# get path directory get_prefix(), e.g. /Users/Chaehan or /home/chaehan
-get_prefix <- function()
+# get path directory path.base, e.g. /Users/Chaehan or /home/chaehan
+get_path_base <- function()
 {
   prefix <- gsub("^(/.*/.*?)/.*", "\\1", getwd())
-  # if (prefix != "/Users/Chaehan")
-  # {
-  #   prefix <- "/home/chaehan"
-  # }
-  return(prefix)
+  if (prefix == "/Users/Chaehan" | prefix == "/private/var")
+  {
+    output <- "/Users/Chaehan"
+  } else {
+    output <- "/home/chaehan"
+  }
+  return(output)
 }
 
-get_prefix()
-
-eval(parse(text = "get_prefix()"))
+path.base <- get_path_base()
 
 # set directories
-credentialsDir <- paste0(get_prefix(), "/Dropbox/06 Machine Learning/06 ML Scripts/_credentials")
-inputML <- paste0(get_prefix(), "/Dropbox/06 Machine Learning/06 ML Scripts/_data/")
-outputML <- paste0(get_prefix(), "/Dropbox/06 Machine Learning/06 ML Scripts/_output/")
-inputDV <- paste0(get_prefix(), "/Google Drive/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_input")
-outputDV <- paste0(get_prefix(), "/Google Drive/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_output")
-inputTP <- paste0(get_prefix(), "/Google Drive/03 Publishing/21 Time Pressure/_input")
-outputTP <- paste0(get_prefix(), "/Google Drive/03 Publishing/21 Time Pressure/_output")
+credentialsDir <- paste0(path.base, "/Dropbox/06 Machine Learning/06 ML Scripts/_credentials")
+inputML <- paste0(path.base, "/Dropbox/06 Machine Learning/06 ML Scripts/_data/")
+outputML <- paste0(path.base, "/Dropbox/06 Machine Learning/06 ML Scripts/_output/")
+inputDV <- paste0(path.base, "/Google Drive/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_input")
+outputDV <- paste0(path.base, "/Google Drive/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_output")
+inputTP <- paste0(path.base, "/Google Drive/03 Publishing/21 Time Pressure/_input")
+outputTP <- paste0(path.base, "/Google Drive/03 Publishing/21 Time Pressure/_output")
 
 
 dirs <- list(credentialsDir,
