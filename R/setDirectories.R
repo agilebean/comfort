7de0fdb16aa50c66d5c153c51f164893e072f761
Agile Bean <chaehan.so@gmail.com>
2018-02-12 17:02:36 +0900

    moved scripts from Dropbox to Google Drive

--- R/setDirectories.R
+++ R/setDirectories.R
@@ -15,9 +15,9 @@ get_path_base <- function()
 path.base <- get_path_base()
 
 # set directories
-credentialsDir <- paste0(path.base, "/Dropbox/06 Machine Learning/06 ML Scripts/_credentials")
-inputML <- paste0(path.base, "/Dropbox/06 Machine Learning/06 ML Scripts/_data/")
-outputML <- paste0(path.base, "/Dropbox/06 Machine Learning/06 ML Scripts/_output/")
+credentialsDir <- paste0(path.base, "/Google Drive/06 Machine Learning/06 ML Scripts/_credentials")
+inputML <- paste0(path.base, "/Google Drive/06 Machine Learning/06 ML Scripts/_data/")
+outputML <- paste0(path.base, "/Google Drive/06 Machine Learning/06 ML Scripts/_output/")
 inputDV <- paste0(path.base, "/Google Drive/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_input")
 outputDV <- paste0(path.base, "/Google Drive/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_output")
 inputTP <- paste0(path.base, "/Google Drive/03 Publishing/21 Time Pressure/_input")
