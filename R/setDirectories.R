e44095b0bd40f277d9a938d576d06144f62456ff
Agile Bean <chaehan.so@gmail.com>
2018-02-12 17:10:46 +0900

    added "06 Data Analysis" in file path

--- R/setDirectories.R
+++ R/setDirectories.R
@@ -15,13 +15,13 @@ get_path_base <- function()
 path.base <- get_path_base()
 
 # set directories
-credentialsDir <- paste0(path.base, "/Google Drive/06 Machine Learning/06 ML Scripts/_credentials")
-inputML <- paste0(path.base, "/Google Drive/06 Machine Learning/06 ML Scripts/_data/")
-outputML <- paste0(path.base, "/Google Drive/06 Machine Learning/06 ML Scripts/_output/")
-inputDV <- paste0(path.base, "/Google Drive/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_input")
-outputDV <- paste0(path.base, "/Google Drive/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_output")
-inputTP <- paste0(path.base, "/Google Drive/03 Publishing/21 Time Pressure/_input")
-outputTP <- paste0(path.base, "/Google Drive/03 Publishing/21 Time Pressure/_output")
+credentialsDir <- paste0(path.base, "/Google Drive/06 Data Analysis/06 Machine Learning/06 ML Scripts/_credentials")
+inputML <- paste0(path.base, "/Google Drive/06 Data Analysis/06 Machine Learning/06 ML Scripts/_data/")
+outputML <- paste0(path.base, "/Google Drive/06 Data Analysis/06 Machine Learning/06 ML Scripts/_output/")
+inputDV <- paste0(path.base, "/Google Drive/06 Data Analysis/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_input")
+outputDV <- paste0(path.base, "/Google Drive/06 Data Analysis/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_output")
+inputTP <- paste0(path.base, "/Google Drive/06 Data Analysis/03 Publishing/21 Time Pressure/_input")
+outputTP <- paste0(path.base, "/Google Drive/06 Data Analysis/03 Publishing/21 Time Pressure/_output")
 
 
 dirs <- list(credentialsDir,
@@ -30,7 +30,4 @@ dirs <- list(credentialsDir,
              inputTP, outputTP)
 
 lapply(dirs, function(dir) assign(toString(dir), dir, envir = .GlobalEnv))
-
-
-
 
