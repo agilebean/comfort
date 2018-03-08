3ff42227910f2037c9f3d71c50a9d5721831e4dd
agilebean <chaehan.so@gmail.com>
2018-03-08 19:15:15 +0900

    corrected inputTP, outputTP paths

--- R/setDirectories.R
+++ R/setDirectories.R
@@ -18,10 +18,10 @@ path.base <- get_path_base()
 credentialsDir <- paste0(path.base, "/Google Drive/06 Data Analysis/06 Machine Learning/06 ML Scripts/_credentials")
 inputML <- paste0(path.base, "/Google Drive/06 Data Analysis/06 Machine Learning/06 ML Scripts/_data/")
 outputML <- paste0(path.base, "/Google Drive/06 Data Analysis/06 Machine Learning/06 ML Scripts/_output/")
-inputDV <- paste0(path.base, "/Google Drive/06 Data Analysis/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_input")
-outputDV <- paste0(path.base, "/Google Drive/06 Data Analysis/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_output")
-inputTP <- paste0(path.base, "/Google Drive/06 Data Analysis/03 Publishing/21 Time Pressure/_input")
-outputTP <- paste0(path.base, "/Google Drive/06 Data Analysis/03 Publishing/21 Time Pressure/_output")
+inputDV <- paste0(path.base, "/Google Drive/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_input")
+outputDV <- paste0(path.base, "/Google Drive/01 Lectures/WS2017 IDAS Lectures/WS17-02 Data Visualization/Datavis Exercises/_output")
+inputTP <- paste0(path.base, "/Google Drive/03 Publishing/21 Time Pressure/_input")
+outputTP <- paste0(path.base, "/Google Drive/03 Publishing/21 Time Pressure/_output")
 
 
 dirs <- list(credentialsDir,
