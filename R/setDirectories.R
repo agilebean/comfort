4d7a6b6bb73ecccc8f543dbd3941119add9ab9d7
agilebean <chaehan.so@gmail.com>
2018-03-23 13:10:19 +0900

    streamlined all diverse script paths under one new folder "03 Data Analysis" cleaned from clutter

--- R/setDirectories.R
+++ R/setDirectories.R
@@ -5,9 +5,9 @@ get_path_base <- function()
   prefix <- gsub("^(/.*/.*?)/.*", "\\1", getwd())
   if (prefix == "/Users/Chaehan" | prefix == "/private/var")
   {
-    output <- "/Users/Chaehan"
+    output <- "/Users/Chaehan/06 Data Analysis"
   } else {
-    output <- "/home/chaehan"
+    output <- prefix
   }
   return(output)
 }
@@ -15,11 +15,11 @@ get_path_base <- function()
 path.base <- get_path_base()
 
 # set directories
-credentialsDir <- paste0(path.base, "/Google Drive/06 Data Analysis/06 Machine Learning/06 ML Scripts/_credentials")
-inputML <- paste0(path.base, "/Google Drive/06 Data Analysis/06 Machine Learning/06 ML Scripts/_data/")
-outputML <- paste0(path.base, "/Google Drive/06 Data Analysis/06 Machine Learning/06 ML Scripts/_output/")
-inputDV <- paste0(path.base, "/Users/Chaehan/Google Drive/06 Data Analysis/06 Data Visualization/_input")
-outputDV <- paste0(path.base, "/Users/Chaehan/Google Drive/06 Data Analysis/06 Data Visualization/_output")
+credentialsDir <- paste0(path.base, "/03 ML Scripts/_credentials")
+inputML <- paste0(path.base, "/03 ML Scripts/_data/")
+outputML <- paste0(path.base, "/03 ML Scripts/_output/")
+inputDV <- paste0(path.base, "/03 Datavis Scripts/_input")
+outputDV <- paste0(path.base, "/03 Datavis Scripts/_output")
 
 dirs <- list(credentialsDir,
              inputML, outputML,
