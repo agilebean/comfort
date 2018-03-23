fcfea5fa256363412db2106e76964c71b6f09c1f
agilebean <chaehan.so@gmail.com>
2018-03-23 13:53:32 +0900

    updated paths to new scripts folder, added EEG paths

--- R/setDirectories.R
+++ R/setDirectories.R
@@ -2,29 +2,31 @@
 # get path directory path.base, e.g. /Users/Chaehan or /home/chaehan
 get_path_base <- function()
 {
-  prefix <- gsub("^(/.*/.*?)/.*", "\\1", getwd())
-  if (prefix == "/Users/Chaehan" | prefix == "/private/var")
-  {
-    output <- "/Users/Chaehan/06 Data Analysis"
-  } else {
-    output <- prefix
-  }
-  return(output)
+  # prefix <- gsub("^(/.*/.*?)/.*", "\\1", getwd())
+  prefix <- gsub("^(/.*/.*/.*?)/.*", "\\1", getwd())
+  prefix <- paste0(prefix, "/03 Data Analysis")
+  return(prefix)
 }
 
 path.base <- get_path_base()
 
 # set directories
 credentialsDir <- paste0(path.base, "/03 ML Scripts/_credentials")
-inputML <- paste0(path.base, "/03 ML Scripts/_data/")
+inputML <- paste0(path.base, "/03 ML Scripts/_input")
 outputML <- paste0(path.base, "/03 ML Scripts/_output/")
+
+inputEEG <- paste0(path.base, "/03 EEG Scripts/_input")
+outputEEG <- paste0(path.base, "/03 EEG Scripts/_output")
+
 inputDV <- paste0(path.base, "/03 Datavis Scripts/_input")
 outputDV <- paste0(path.base, "/03 Datavis Scripts/_output")
 
+
 dirs <- list(credentialsDir,
              inputML, outputML,
-             inputDV, outputDV,
-             inputTP, outputTP)
+             inputEEG, outputEEG,
+             inputDV, outputDV
+             )
 
 lapply(dirs, function(dir) assign(toString(dir), dir, envir = .GlobalEnv))
 
