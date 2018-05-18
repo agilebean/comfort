d886f61fd10d1fd0b9cf593c87b6586be2cf44d7
agilebean <chaehan.so@gmail.com>
2018-05-18 17:30:54 +0900

    eliminated "/03 Data Analysis" from path.base to enable flexible paths

--- R/setDirectories.R
+++ R/setDirectories.R
@@ -16,25 +16,25 @@ get_path_base <- function()
     }
   }
 
-  prefix <- paste0(prefix, "/03 Data Analysis")
+  # prefix <- paste0(prefix, "/03 Data Analysis")
   return(prefix)
 }
 
 path.base <- get_path_base()
 
 # set directories
-credentialsDir <- paste0(path.base, "/03 ML Scripts/_credentials/")
-inputML <- paste0(path.base, "/03 ML Scripts/_input/")
-outputML <- paste0(path.base, "/03 ML Scripts/_output/")
+credentialsDir <- paste0(path.base, "/03 Data Analysis/03 ML Scripts/_credentials/")
+inputML <- paste0(path.base, "/03 Data Analysis/03 ML Scripts/_input/")
+outputML <- paste0(path.base, "/03 Data Analysis/03 ML Scripts/_output/")
 
-inputEEG <- paste0(path.base, "/03 EEG Scripts/_input/")
-outputEEG <- paste0(path.base, "/03 EEG Scripts/_output/")
+inputEEG <- paste0(path.base, "/03 Data Analysis/03 EEG Scripts/_input/")
+outputEEG <- paste0(path.base, "/03 Data Analysis/03 EEG Scripts/_output/")
 
-inputDV <- paste0(path.base, "/03 Datavis Scripts/_input/")
-outputDV <- paste0(path.base, "/03 Datavis Scripts/_output/")
+inputDV <- paste0(path.base, "/03 Data Analysis/03 Datavis Scripts/_input/")
+outputDV <- paste0(path.base, "/03 Data Analysis/03 Datavis Scripts/_output/")
 
 inputTimex <- paste0(path.base, "/04 Publishing/21 Timex/_input/")
-outputTimex <- paste0(path.base, "04 Publishing/21 Timex/_output/")
+outputTimex <- paste0(path.base, "/04 Publishing/21 Timex/_output/")
 
 dirs <- list(credentialsDir,
              inputML, outputML,
